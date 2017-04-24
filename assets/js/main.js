78e2406f69cd8aa0c8769a1250625b7eb916ae60
Caromin <c.aromin.unf@gmail.com>
2017-04-24 02:39:09 -0400

    99% completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 var random = [ "twice", "aoa", "gfriend", "pristin", "running man", "knowing bros"]
 var losses = 0;
 var wins = 0;
-var livesRemaining = 10;
+var livesRemaining = 0;
 var word = "";
 var answer = [];
 var underScoreArray = [];
@@ -35,34 +35,47 @@ function generateUnderscore(word) {
 	}
 
 // checks the keypress and updates the info if the letter exists
-function replacingDisplay(which) {
+function replacingDisplay(which) {	
 	var check = false;
-	var event = String.fromCharCode(which);
+// set CharCode to variable AND also making it lowercase automatically 	
+	var event = String.fromCharCode(which).toLowerCase();
 	for (i = 0; i <answer.length; i++) {
 		if (event === answer[i]) {
 			console.log("yes it matches!");
 			underScoreArray.splice(i, 1, event);
 			document.getElementById("hangmanWord").innerHTML= underScoreArray.join(" ");
+// if there is a match in the array, then check becomes true			
 		check = true;
 		} 
 	}
+// if check is TRUE then this will run	
 	if (check) {
+// comparing the answer.join with the updating underScoreArray.join		
 		if (answer.join(" ") === underScoreArray.join(" ")) {
 			wins++;
 			$('#totalWins').html(wins);
 			alert("You won! The answer was " + word + "!");
+			$('#hangmanWord').addClass("text-success");
+// setting it equal to 0 so that the keypress below will not work anymore			
 			livesRemaining = 0;
-		} 
+		}
+// if check is still FALSE, then this will run
 	} else if (!check) {
-		livesRemaining--;
-		guessesArray.push(event);
-		$('#answerDiv').html(guessesArray.join(", "));
-		$('#answerDiv').addClass("text-danger h3");
-		$('#livesLeft').html(livesRemaining);
-		if (livesRemaining === 0 ) {
-			alert("Game over! answer was " + word);
-			losses++;
-			$('#totalLosses').html(losses);
+// checks the guessesArray for the event variable, if it is not here then the result is -1 === -1		
+		if ( $.inArray(event, guessesArray) === -1) {
+			livesRemaining--;
+			guessesArray.push(event);
+			$('#answerDiv').html(guessesArray.join(", "));
+			$('#answerDiv').addClass("text-danger h3");
+			$('#livesLeft').html(livesRemaining);
+// if they have no livesRemaining this will run			
+			if (livesRemaining === 0 ) {
+				alert("Game over! answer was " + word);
+				$('#hangmanWord').html(answer.join(" "));
+				$('#hangmanWord').addClass("text-danger");
+				losses++;
+				$('#totalLosses').html(losses);
+		}
 		}
 	}
 }
@@ -74,7 +87,10 @@ function RestartGame() {
 	generateUnderscore(word);
 	livesRemaining = 10;
 	$('#livesLeft').html(livesRemaining);
-	$('#answerDiv').html(" ");
+	$('#answerDiv').html("");
+	guessesArray = [];
+
+	$("#hangmanWord").removeClass("text-danger text-success");
 	console.log(answer);
 }
 
@@ -89,10 +105,12 @@ $('#restartGame').on('click', function() {
 	RestartGame();
 });
 
+// this runs the game key press and when there is no lives remainging, the keypress function will stop
 $(window).keypress(function(event){
 	if (livesRemaining === 0) {
 		return false;
 	} else {
+// this is the function that will check and update the display		
 	replacingDisplay(event.which); 
 	}
 });
